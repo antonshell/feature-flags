67ffa1391618b5be0797d954f62a1667a3d1453d
Anton Shelyganov <antonshell@yandex.ru>
2023-03-16 21:44:46 +0400

    Fixed codestyle

--- src/Entity/TimestampTrait.php
+++ src/Entity/TimestampTrait.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\MappedSuperclass]
@@ -12,17 +11,17 @@ use Doctrine\ORM\Mapping as ORM;
 trait TimestampTrait
 {
     #[ORM\Column(type: 'datetime', nullable: true)]
-    private ?DateTimeInterface $createdAt = null;
+    private ?\DateTimeInterface $createdAt = null;
 
     #[ORM\Column(type: 'datetime', nullable: true, columnDefinition: 'DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP')]
-    private ?DateTimeInterface $updatedAt = null;
+    private ?\DateTimeInterface $updatedAt = null;
 
-    public function getCreatedAt(): ?DateTimeInterface
+    public function getCreatedAt(): ?\DateTimeInterface
     {
         return $this->createdAt;
     }
 
-    public function getUpdatedAt(): ?DateTimeInterface
+    public function getUpdatedAt(): ?\DateTimeInterface
     {
         if (null === $this->updatedAt) {
             return $this->getCreatedAt();
