aa0565301cfd43b3db90ceaf0347489e9fe13dd8
Anton Shelyganov <antonshell@yandex.ru>
2023-03-16 20:07:22 +0400

    Fixed deprecation warning

--- src/EventSubscriber/TokenSubscriber.php
+++ src/EventSubscriber/TokenSubscriber.php
@@ -71,7 +71,7 @@ class TokenSubscriber implements EventSubscriberInterface
     /**
      * @return array
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             KernelEvents::CONTROLLER => 'onKernelController',
